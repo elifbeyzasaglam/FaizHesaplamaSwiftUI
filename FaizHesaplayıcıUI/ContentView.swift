0c470f4eaedc0f346c0114f4b42773e76c0058fb
Elif Beyza <saglame99@gmail.com>
2022-08-12 21:54:29 +0300

    first commit

--- FaizHesaplayıcıUI/ContentView.swift
+++ FaizHesaplayıcıUI/ContentView.swift
@@ -8,9 +8,63 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
+    @State var anaPara = 0
+    
+    let formatter:NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = Locale(identifier: "tr_TR")
+        return formatter
+    }()
+    //Dictionary
+    let faizOranlar: [Int:Double] = [3:0.2, 6:0.25, 9:0.30 , 12:0.50]
+    @State var seçilenVade = 3
+    var toplamPara:String{
+        let faizGetirisi = Double(anaPara) * faizOranlar[seçilenVade]!
+        
+        let toplamGetiri = formatter.string(from: NSNumber(value:faizGetirisi + Double(anaPara)) )
+        return toplamGetiri!
+        
+    }
+        var body: some View {
+        VStack{
+            Image("ziraat")
+                .resizable()
+                .scaledToFit()
+                .frame(width:220)
+            
+            Text("Ana Para")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(.red)
+            HStack{
+                TextField("0 TL",value:$anaPara, formatter:formatter)
+                    .keyboardType(.numberPad)
+                    .padding()
+                    .frame(height:60)
+                    .background(Color.gray.opacity(0.2))
+                    .cornerRadius(10)
+            }
             .padding()
+            
+            Text("Vade Seçiniz")
+                .font(.title2)
+                .fontWeight(.bold)
+                .foregroundColor(.red)
+            
+            Picker("Vade Seçiniz", selection: $seçilenVade){
+                ForEach(Array(faizOranlar.keys.sorted()),id: \.self){ item in
+                Text("\(item)")
+                }
+            }.pickerStyle(.segmented)
+                .padding()
+           
+            Spacer()
+            
+            Text(toplamPara)
+            
+        }
+        
     }
 }
 
