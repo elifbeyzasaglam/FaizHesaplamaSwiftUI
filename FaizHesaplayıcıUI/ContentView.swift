de5885b31506064e997363dc85dfefc01a0613bc
Elif Beyza <saglame99@gmail.com>
2022-08-12 16:55:37 +0300

    Initial Commit

---
+++ FaizHesaplayıcıUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FaizHesaplayıcıUI
+//
+//  Created by ELİF BEYZA SAĞLAM on 12.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FaizHesaplayıcıUI/FaizHesaplay_c_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaizHesaplay_c_UIApp.swift
+//  FaizHesaplayıcıUI
+//
+//  Created by ELİF BEYZA SAĞLAM on 12.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct FaizHesaplay_c_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
